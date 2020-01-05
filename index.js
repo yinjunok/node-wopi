90fcdf83bce17baaeb6dcec9a9b5dbadb152fbfd
yinjun <yinjunok@foxmail.com>
2020-01-05 11:47:03 +0800

    modify port

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app
   .use(router.routes())
   .use(router.allowedMethods())
 
-app.listen(3000, () => {
-  console.log('[demo] route-use-middleware is starting at port 3000')
+app.listen(8080, () => {
+  console.log('[demo] route-use-middleware is starting at port 8080')
 })
 
