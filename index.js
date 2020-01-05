02ee8f6ec5dc71c8b8f1f2fd82432596bb69dcc6
yinjun <yinjunok@foxmail.com>
2020-01-05 11:45:14 +0800

    demo

---
+++ index.js
@@ -0,0 +1,40 @@
+const fs = require('fs')
+const path = require('path')
+const Koa = require('koa')
+const Hash = require('hash.js')
+const Router = require('@koa/router')
+
+const app = new Koa()
+const router = new Router()
+
+router.get('/files/:name', (ctx, next) => {
+  const name = ctx.params.name
+  const p = path.resolve('./office', name)
+  const stat = fs.statSync(p)
+  const file = fs.readFileSync(p, 'utf8')
+  ctx.body = {
+    BaseFileName: name,
+    OwnerId: 'admin',
+    Size: stat.size,
+    SHA256: Hash.sha256().update(file).digest('hex'),
+    Version: Date.now(),
+  }
+  ctx.response.set('Content-Type', 'application/json')
+})
+
+router.post('/files/:name/contents', (ctx, next) => {
+  const name = ctx.params.name
+  const p = path.resolve('./office', name)
+  const s = fs.createReadStream(p)
+  ctx.response.set("Content-type", "application/octet-stream")
+  ctx.body = s
+})
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+app.listen(3000, () => {
+  console.log('[demo] route-use-middleware is starting at port 3000')
+})
+
