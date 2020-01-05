47838e6a9659d25548cf2702ed8d6b1efe4e51bb
yinjun <yinjunok@foxmail.com>
2020-01-05 12:26:36 +0800

    error log

--- index.js
+++ index.js
@@ -8,31 +8,41 @@ const app = new Koa()
 const router = new Router()
 
 router.get('/wopi/files/:name', (ctx, next) => {
-  const name = ctx.params.name
-  const p = path.resolve('./office', name)
-  const stat = fs.statSync(p)
-  const file = fs.readFileSync(p, 'utf8')
-  console.log('返回文件信息', name)
-  ctx.body = {
-    BaseFileName: name,
-    OwnerId: 'admin',
-    Size: stat.size,
-    SHA256: Hash.sha256().update(file).digest('hex'),
-    Version: Date.now(),
+  try {
+    const name = ctx.params.name
+    const p = path.resolve('./office', name)
+    const stat = fs.statSync(p)
+    const file = fs.readFileSync(p, 'utf8')
+    console.log(Date.now(), '返回文件信息', name)
+    ctx.body = {
+      BaseFileName: name,
+      OwnerId: 'admin',
+      Size: stat.size,
+      SHA256: Hash.sha256().update(file).digest('hex'),
+      Version: Date.now(),
+    }
+    ctx.response.set('Content-Type', 'application/json')
+  } catch (err) {
+    console.log(Date.now(), '返回文件信息错误', err)
+    throw err
   }
-  ctx.response.set('Content-Type', 'application/json')
 })
 
 router.post('/wopi/files/:name/contents', (ctx, next) => {
-  const name = ctx.params.name
-  const p = path.resolve('./office', name)
-  const s = fs.createReadStream(p)
-  console.log('返回文件流', name)
-  const stat = fs.statSync(p)
-  ctx.response.set("Content-type", "application/octet-stream")
-  ctx.response.set('Content-Length', stat.size)
-  ctx.response.set('Content-Disposition', `attachment;filename=${name}`)
-  ctx.body = s
+  try {
+    const name = ctx.params.name
+    const p = path.resolve('./office', name)
+    const s = fs.createReadStream(p)
+    console.log(Date.now(), '返回文件流', name)
+    const stat = fs.statSync(p)
+    ctx.response.set("Content-type", "application/octet-stream")
+    ctx.response.set('Content-Length', stat.size)
+    ctx.response.set('Content-Disposition', `attachment;filename=${name}`)
+    ctx.body = s
+  } catch (err) {
+    console.log(Date.now(), '返回文件信息流', err)
+    throw err
+  }
 })
 
 app
